24eb3ad0cc2db06d3f08822401750fcc0afc1fd7
G00402583 <92363325+G00402583@users.noreply.github.com>
2025-05-12 21:10:01 +0100

    Update supaClient.js

--- js/supaClient.js
+++ js/supaClient.js
@@ -1,7 +1,7 @@
 //  reusable Supabase client across all pages
 
-export const SB_URL = "https://kqzevnsdurpptiaxszqq.supabase.co";
-export const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxemV2bnNkdXJwcHRpYXhzenFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwMjA4NDYsImV4cCI6MjA1OTU5Njg0Nn0.eM-M08VulR5HiwKs-t7y2xehqUwBJPW0dnKENxMvArg";
+export const SB_URL = "#";
+export const SB_KEY = "#";
   //  anon key
 
 export const sb = supabase.createClient(SB_URL, SB_KEY);
